04d17b627310e69eeb7e4457fd4cf3c8aa009611
Jester101222 <wolfunder8@gmail.com>
2024-03-22 17:04:51 +0800

    kasiscommit

--- src/info.js
+++ src/info.js
@@ -2,7 +2,13 @@
 const info = (title, para, contactBtn) => {
     let div = document.createElement("div")
     div.className = "ClassZ"
-    div.innerHTML = `<h1>${title}: <small><i>Kentaro Miura</i></small></h1>
+    div.innerHTML = `
+    <nav class="Navc1">
+        <ul class="ul1">
+            <li><a href="https://antidomario.github.io/myport/">Back</a></li>
+        </ul>
+    </nav>
+    <h1>${title}: <small><i>Kentaro Miura</i></small></h1>
                     <hr>
                      <p class="para">${para}</p>
                      <a href="https://en.wikipedia.org/wiki/Kentaro_Miura"><button class="button">${contactBtn}</button></a>
